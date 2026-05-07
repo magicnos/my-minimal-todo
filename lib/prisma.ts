2ee2c8e982133b0bd34fe3acd56bb5449db79815
magicnos <tianzhonghiroki82@gmail.com>
2026-05-08 05:52:33 +0900

    update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
 /**
- * データベース（Prisma）の「窓口」を作るプログラムです。
- * アプリの中で、この窓口（prisma インスタンス）を通じてデータの保存や読み込みを行います。
+ * データベースの「窓口」を作るプログラムです。
  */
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// アプリが動いている間、窓口が何個もできないように工夫しています。
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient();
+  new PrismaClient({
+    // 最新バージョンでは、ここに接続先を渡す必要があります
+    datasourceUrl: process.env.POSTGRES_PRISMA_URL,
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
